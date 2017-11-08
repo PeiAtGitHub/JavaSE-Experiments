ed6a79b52ea11431eae5cdafdd760dff2fc4ed67
pei <pei.shi@outlook.com>
2017-11-09 00:31:20 +0100

    Math Utils

---
+++ JavaProject_JSE_Experiments/src/pei/java/jse/experiments/language/MathUtils.java
@@ -0,0 +1,23 @@
+package pei.java.jse.experiments.language;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class MathUtils {
+	
+	public static void main(String[] args) {
+
+		//
+		System.out.println(Math.round(0.4));
+		System.out.println(Math.round(0.6));
+
+		//
+		for(int i =0; i<=9;i++){
+			System.out.println(Math.random());
+		}
+
+	}
+	
+}
