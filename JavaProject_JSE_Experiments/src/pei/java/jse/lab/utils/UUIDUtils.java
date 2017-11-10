70430825c0abac5efbaefa95bf26a0a623e36677
pei <pei.shi@outlook.com>
2017-11-10 21:00:40 +0100

    UUID utils

---
+++ JavaProject_JSE_Experiments/src/pei/java/jse/lab/utils/UUIDUtils.java
@@ -0,0 +1,20 @@
+package pei.java.jse.lab.utils;
+
+import static org.junit.Assert.assertEquals;
+import java.util.UUID;
+import org.junit.Test;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class UUIDUtils {
+	@Test
+	public void testUUID() {
+		UUID uuid4 = UUID.randomUUID();
+		assertEquals(4, uuid4.version());
+		assertEquals(36, uuid4.toString().length()); // including 4 hyphens
+	}
+
+}
