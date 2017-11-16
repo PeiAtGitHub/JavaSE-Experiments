62a9e7387b0a8c36986a304e0b2be62f36688c8e
pei <pei.shi@outlook.com>
2017-11-16 23:18:06 +0100

    Multi catch exceptions

---
+++ Java_SE_Lab/src/pei/java/jse/lab/java7new/MultiCatch.java
@@ -0,0 +1,37 @@
+package pei.java.jse.lab.java7new;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+import org.junit.Test;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class MultiCatch {
+	
+	@Test
+	public void testMultiCatch(){
+		Random r = new Random();
+		for (int i = 0; i < 5; i++) {
+			try {
+				if(r.nextBoolean()) {
+					System.out.println("About to throw Exception A");
+					throw new ExceptionA();
+				}else {
+					System.out.println("About to throw Exception B");
+					throw new ExceptionB();
+				}
+			} catch (ExceptionA | ExceptionB e) {// a concise way of catching multi exceptions; 
+				assertTrue(e instanceof ExceptionA 
+						|| e instanceof ExceptionB);
+			}
+		}
+	}
+
+}
+
+class ExceptionA extends Exception{}
+class ExceptionB extends Exception{}
