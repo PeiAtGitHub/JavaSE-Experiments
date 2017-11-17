9a212d61371a7bc8ca2f874c1918e74700b1110a
pei <pei.shi@outlook.com>
2017-11-17 23:41:13 +0100

    Properties file

---
+++ Java_SE_Lab/src/pei/java/jse/lab/io/PropertiesFile.java
@@ -0,0 +1,37 @@
+package pei.java.jse.lab.io;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class PropertiesFile {
+	
+	@Test
+	public void testPropertiesFile() throws IOException {
+		Properties props = new Properties();
+		// Properties has another constructor that takes a default Properties object
+		// which has default properties
+		// Demo for this feature is skipped here.
+		
+		FileInputStream in = new FileInputStream("TestProperties.properties");
+		props.load(in);
+		in.close();
+		
+		assertTrue(props.getProperty("p1").equals("abcdefg"));
+		assertTrue(props.getProperty("p2").equals("1234567890"));
+		assertTrue(props.getProperty("p3").isEmpty());
+		assertNull(props.getProperty("NonExistentKey"));
+
+	}
+
+}
