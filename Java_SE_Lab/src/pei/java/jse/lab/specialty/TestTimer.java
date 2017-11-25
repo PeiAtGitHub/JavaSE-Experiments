c0f5204b7273609d45062475a443b1cbf3074e2b
pei <pei.shi@outlook.com>
2017-11-25 23:30:55 +0100

    Timer and Timer task

---
+++ Java_SE_Lab/src/pei/java/jse/lab/specialty/TestTimer.java
@@ -0,0 +1,53 @@
+package pei.java.jse.lab.specialty;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class TestTimer {
+	
+		@Test
+		public void timerAndTimerTask() {
+
+			Timer timer1 = new Timer();             
+			timer1.schedule(new Task("T1"), 0, 5000);
+
+			Timer timer2 = new Timer();             
+			timer2.schedule(new Task("T2"), 0, 3000);
+			
+			try {
+				Thread.sleep(15000); // 15s
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			timer1.cancel();
+			timer2.cancel();
+			
+		}
+
+}
+
+// TimerTask implemented Runnable
+class Task extends TimerTask {
+	
+	private String taskName;
+	private int runCounter = 0;
+
+	Task(String tasktName) {
+		this.taskName = tasktName;
+		runCounter = 0;
+	}
+
+	public void run() {
+		runCounter++;
+		System.out.format("Task %s runs x%s%n", taskName, runCounter); 
+	}
+
+}
