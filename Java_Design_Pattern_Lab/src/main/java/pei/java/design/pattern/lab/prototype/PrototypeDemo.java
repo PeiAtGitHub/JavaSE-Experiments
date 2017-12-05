b434dfd33a6928f8328352d9d83cd3ccd088c2af
pei <pei.shi@outlook.com>
2017-12-05 01:02:42 +0100

    DP: prototype; threading

---
+++ Java_Design_Pattern_Lab/src/main/java/pei/java/design/pattern/lab/prototype/PrototypeDemo.java
@@ -0,0 +1,59 @@
+package pei.java.design.pattern.lab.prototype;
+
+/**
+ * 
+ * 
+ * @author pei
+ *
+ */
+public class PrototypeDemo {
+
+	public static void main(String args[]) throws CloneNotSupportedException {
+		Prototype prototype = new PrototypeImpl(10);
+
+		for (int i = 0; i < 10; i++) {
+			// new object created from prototype has same value as prototype
+			Prototype newObj= prototype.clone();
+			
+			// customize the new created object
+			newObj.setX(newObj.getX() * (i+1));
+			newObj.printX();
+		}
+	}
+}
+
+//
+class PrototypeImpl extends Prototype {
+	int x;
+
+	public PrototypeImpl(int x) {
+		this.x = x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void printX() {
+		System.out.println("Value: " + x);
+	}
+}
+
+abstract class Prototype implements Cloneable {
+	@Override
+	public Prototype clone() throws CloneNotSupportedException {
+		// Default clone() is shallow copy, which is adequate
+		// for this case.
+		return (Prototype) super.clone();
+	}
+
+	public abstract void setX(int x);
+
+	public abstract int getX();
+
+	public abstract void printX();
+}
---
+++ Java_SE_Lab/src/pei/java/jse/lab/threading/CallableAndFuture.java
@@ -0,0 +1,64 @@
+package pei.java.jse.lab.threading;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+
+/**
+ * A Future represents the result of an asynchronous computation.
+ * 
+ * @author pei
+ *
+ */
+
+public class CallableAndFuture {
+
+	final static String RETURNED_TEXT = "Callable Task Finished";
+
+	@Test
+	public void basics() throws ExecutionException, InterruptedException {
+		ExecutorService threadPool = Executors.newFixedThreadPool(1);
+
+		Callable<String> theTask = new CallableTask();
+		Future<String> future = threadPool.submit(theTask);
+		assertTrue(future.get().equals(RETURNED_TEXT));// wait until task finished.
+
+		// Callable task can be reused
+		// FutureTask implements the Future interface.
+		FutureTask<String> fTask = new FutureTask<String>(theTask);
+		threadPool.submit(fTask);
+		assertFalse(fTask.isDone());
+		assertTrue(fTask.get().equals(RETURNED_TEXT));
+
+		threadPool.shutdown();
+
+		//
+		future = Executors.newCachedThreadPool().submit(theTask);
+		try {
+			future.get(3, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			assertTrue(e instanceof TimeoutException);
+			future.cancel(true);
+			assertTrue(future.isCancelled());
+			assertTrue(future.isDone());
+		}
+	}
+}
+
+//
+class CallableTask implements Callable<String> {
+	public String call() throws Exception {
+		Thread.sleep(5000);// 5s
+		return CallableAndFuture.RETURNED_TEXT;
+	}
+}
