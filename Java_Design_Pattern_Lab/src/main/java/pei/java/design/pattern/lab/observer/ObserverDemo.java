1c4cb3121b69b988e8ee9c395343f1566e7a1997
pei <pei.shi@outlook.com>
2018-01-06 15:53:00 +0100

    Design Pattern: observer

---
+++ Java_Design_Pattern_Lab/src/main/java/pei/java/design/pattern/lab/observer/ObserverDemo.java
@@ -0,0 +1,63 @@
+package pei.java.design.pattern.lab.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class ObserverDemo {
+
+    public static void main(String[] args) {
+ 
+        final TheSubject eventSource = new TheSubject();
+
+        eventSource.addObserver(new TheObserver("Observer1"));
+        eventSource.addObserver(new TheObserver("Observer2"));
+        
+        eventSource.whatever();
+        eventSource.whatever();
+        eventSource.whatever();
+        
+    }
+
+}
+
+//
+
+@ToString
+class TheSubject extends Observable {
+	
+	private int counter = 0;
+	
+	public void whatever() {
+		counter++;
+		setChanged();
+		notifyObservers(new Event("Counter changed to: " + counter));
+	}
+	
+}
+
+@ToString @AllArgsConstructor @Slf4j
+class TheObserver implements Observer {
+    
+	String name;
+	
+    public void update(Observable obj, Object arg) {
+        log.info("{} got sth({}) from {}", 
+        		this.toString(), arg.toString(), obj.toString());
+    }
+    
+}
+
+@AllArgsConstructor @ToString
+class Event {
+	String description;
+}
+
