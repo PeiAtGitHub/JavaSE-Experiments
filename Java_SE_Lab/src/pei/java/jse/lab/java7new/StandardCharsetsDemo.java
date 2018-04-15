92d87d218f0f927e857383a9460c3861ef84656e
pei <pei.shi@outlook.com>
2018-04-15 22:26:11 +0200

    StandardCharsets and Charset

---
+++ Java_SE_Lab/src/pei/java/jse/lab/java7new/StandardCharsetsDemo.java
@@ -0,0 +1,28 @@
+package pei.java.jse.lab.java7new;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public class StandardCharsetsDemo {
+
+	public static void main(String[] args) {
+		
+		// StandardCharsets is new in Java 7
+		System.out.println(StandardCharsets.ISO_8859_1.name());
+		System.out.println(StandardCharsets.US_ASCII.name());
+		System.out.println(StandardCharsets.UTF_16.name());
+		System.out.println(StandardCharsets.UTF_16BE.name());
+		System.out.println(StandardCharsets.UTF_16LE.name());
+		System.out.println(StandardCharsets.UTF_8.name());
+		// Charset is available since 1.4
+		System.out.println(Charset.isSupported(StandardCharsets.US_ASCII.name()));
+		System.out.println(Charset.availableCharsets());
+		System.out.println(Charset.defaultCharset());
+		
+	}
+}
