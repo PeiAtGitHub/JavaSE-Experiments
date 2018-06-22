9bf9cadcac5e4bd4ea66672f88a4f82665378806
pei <pei.shi@outlook.com>
2018-06-23 01:02:36 +0200

    Logging String format demo

---
+++ Java_SE_Lab/src/pei/java/jse/lab/specialty/LoggingDemos.java
@@ -0,0 +1,29 @@
+package pei.java.jse.lab.specialty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author pei
+ *
+ */
+@Slf4j
+public class LoggingDemos {
+    
+    @Test
+    public void loggerFormat() throws Exception {
+        
+        log.info("Hello, {} and {}.");
+        log.info("Hello, {} and {}.", "Tom");
+        log.info("Hello, {} and {}.", "Tom", "Jerry");
+        log.info("Hello, {} and {}.", "Tom", "Jerry", "Some one");
+        log.info("Hello, { } and {}.", "Tom", "Jerry", "Some one");
+        log.info("Hello, \\{} and {}.", "Tom", "Jerry", "Some one");
+        
+    }
+
+}
